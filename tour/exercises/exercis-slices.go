6cdfde1020ebde9405efe158c62c935e784b1408
Pi Chiu <rtchiou@gmail.com>
2023-01-17 11:29:27 +0800

    Rewrite slice exercise with make

--- tour/exercises/exercis-slices.go
+++ tour/exercises/exercis-slices.go
@@ -3,16 +3,15 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-	var ret [][]uint8
-	for i := 0 ; i < dy; i++ {
-		s := make([]uint8, dx)
-		for j, _ := range(s) {
-			// s[j] = uint8((i + j)/2)
-			s[j] = uint8(i*j)
+	s := make([][]uint8, dy)
+	for i, _ := range(s) {
+		s[i] = make([]uint8, dx)
+		for j, _ := range(s[i]) {
+			// s[i][j] = uint8((i + j)/2)
+			s[i][j] = uint8(i^j)
 		}
-		ret = append(ret, s)
 	}
-	return ret
+	return s
 }
 
 func main() {
