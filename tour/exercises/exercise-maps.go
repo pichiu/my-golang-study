49bbeb02658563d38c596e26b7f400dbf1a63af9
Pi Chiu <rtchiou@gmail.com>
2023-01-17 11:51:07 +0800

    Remove count

--- tour/exercises/exercise-maps.go
+++ tour/exercises/exercise-maps.go
@@ -8,8 +8,7 @@ import (
 func WordCount(s string) map[string]int {
     m := make(map[string]int)
 	for _, v := range strings.Fields(s) {
-	    count := m[v]
-		m[v] = count + 1
+	    m[v]++
 	}
 	return m
 }
