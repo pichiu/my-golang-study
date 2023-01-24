17d396e0dd7a73472a7ea061c6919a855f13eae1
pi_chiu <rtchiou@gmail.com>
2023-01-25 00:07:50 +0800

    [0088] Reduce variables

--- leetcode/0088-Merge_Sorted_Array/merge.go
+++ leetcode/0088-Merge_Sorted_Array/merge.go
@@ -1,9 +1,7 @@
 package test
 
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    t := m+n-1
-
-    for m > 0 && n > 0 {
+    for t := m+n-1; m > 0 && n > 0; t-- {
         if nums1[m-1] > nums2[n-1] {
             nums1[t] = nums1[m-1]
             m--
@@ -11,12 +9,10 @@ func merge(nums1 []int, m int, nums2 []int, n int)  {
             nums1[t] = nums2[n-1]
             n--
         }
-        t--
     }
 
     for n > 0 {
-        nums1[t] = nums2[n-1]
+        nums1[n-1] = nums2[n-1]
         n--
-        t--
     }
 }
