d20079697f426f9e642573c060ba29ce3c7b0d17
pi_chiu <rtchiou@gmail.com>
2023-01-25 00:23:56 +0800

    [0070] fast version

--- leetcode/0070-Climbing_Stairs/climb_stairs.go
+++ leetcode/0070-Climbing_Stairs/climb_stairs.go
@@ -1,15 +1,9 @@
 package test
 
 func climbStairs(n int) int {
-    l := make([]int, n+1)
-	l[0] = 1
-	l[1] = 1
-	if n > 1 {
-		for i := 2; i < n+1; i++ {
-			l[i] = l[i-2] + l[i-1]
-		}
-		return l[n]
-	} else {
-		return 1
+	current, next := 1, 1
+	for i := 1; i < n; i++ {
+		current, next = next, next+current
 	}
+	return next
 }
