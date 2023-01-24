4208b3986753c5cedb1a52894fab2f2d7a03ef3b
pi_chiu <rtchiou@gmail.com>
2023-01-24 23:28:06 +0800

    [0088] fast version

--- leetcode/0088-Merge_Sorted_Array/merge.go
+++ leetcode/0088-Merge_Sorted_Array/merge.go
@@ -1,41 +1,22 @@
 package test
 
-import (
-	"fmt"
-)
-
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-    if  n > 0 {
-		if m == 0 {
-			for i, v := range nums2 {
-				nums1[i] = v
-			}
-		} else {
-			c := n-1
-			t := m+n-1
-			for i := m-1; i >= 0; i-- {
-				for j := c; j >= 0; j-- {
-					if nums2[j] > nums1[i] {
-						nums1[t] = nums2[j]
-						fmt.Println(t, nums1)
-						c--
-						t--
-					} else {
-						nums1[t] = nums1[i]
-						fmt.Println(t, nums1)
-						t--
-						break
-					}
-				}
-			}
-			if c >= 0 {
-				for j := c; j >= 0; j-- {
-					nums1[t] = nums2[j]
-					fmt.Println(t, nums1)
-					c--
-					t--
-				}
-			}
-		}
-	}
+    t := m+n-1
+
+    for m > 0 && n > 0 {
+        if nums1[m-1] > nums2[n-1] {
+            nums1[t] = nums1[m-1]
+            m--
+        } else {
+            nums1[t] = nums2[n-1]
+            n--
+        }
+        t--
+    }
+
+    for n > 0 {
+        nums1[t] = nums2[n-1]
+        n--
+        t--
+    }
 }
