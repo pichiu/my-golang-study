997845827d8346a9563655b8f97264b53e403e7c
pi_chiu <rtchiou@gmail.com>
2023-01-25 11:07:50 +0800

    [0859] check string equal first

--- leetcode/0859-Buddy_Strings/buddy_strings.go
+++ leetcode/0859-Buddy_Strings/buddy_strings.go
@@ -4,28 +4,26 @@ func buddyStrings(s string, goal string) bool {
 	if len(s) != len(goal) {
 		return false
 	}
+	if s == goal {
+		m := make(map[byte]int)
+		for i := 0; i < len(s); i++ {
+			if m[s[i]] == 1 {
+				return true
+			}
+			m[s[i]]++
+		}
+		return false
+	}
 	var diff []int
-	m := make(map[byte]int)
     for i := 0; i < len(s); i++ {
 		if s[i] != goal[i] {
 			diff = append(diff, i)
 		}
-		m[s[i]]++
 	}
-	ret := false
-	if len(diff) == 0 {
-		for _, element := range m {
-			if element > 1 {
-				ret = true
-				break
-			}
-		}
-	} else if len(diff) == 2 {
+	if len(diff) == 2 {
 		r := []rune(s)
-		r[diff[0]], r[diff[1]] = r[diff[1]], r[diff[0]]
-		if string(r) == goal {
-			ret = true
-		}
+		g := []rune(goal)
+		return r[diff[0]] == g[diff[1]] && r[diff[1]] == g[diff[0]]
 	}
-	return ret
+	return false
 }
